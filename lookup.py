309f2950c0951d8b84119f01fa664ce3ff61115b
Sajjad Salaria <imsafay@gmail.com>
2018-02-04 23:05:33 +0530

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -15,6 +15,6 @@ for x in api:
 	for coin in currencies:
 		if coin == x["symbol"]:
 			print(x["name"])
-			print("${0:.2f}".format(float(x["price_usd"]))
-			print("Rank: {0:.0f}".format(float(x["rank"])))
-			print("------------------")
+			print("${0:.2f}".format(float(x["price_usd"])))
+			print("Rank:{0:.0f}".format(float(x["rank"])))
+            		print("------------------")
