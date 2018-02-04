d45a5b27a03f5b132c3170fedc66d3ef6d17ccf9
Sajjad Salaria <imsafay@gmail.com>
2018-02-04 22:52:41 +0530

    Paring with terminal (Basic Functionality)

--- lookup.py
+++ lookup.py
@@ -3,3 +3,18 @@ import json
 
 import os
 os.system('cls')
+###########################################
+
+api_request = requests.get("https://api.coinmarketcap.com/v1/ticker/")
+api = json.loads(api_request.content)
+
+currencies = ["BTC", "XRP", "EQS", "STEEM"]
+
+print("------------------")
+for x in api:
+	for coin in currencies:
+		if coin == x["symbol"]:
+			print(x["name"])
+			print("${0:.2f}".format(float(x["price_usd"]))
+			print"Rank: {0:.0f}".format(float(x["rank"])))
+			print("------------------")
